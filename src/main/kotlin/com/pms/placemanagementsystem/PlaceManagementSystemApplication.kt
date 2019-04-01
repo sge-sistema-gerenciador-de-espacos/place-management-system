12b44675b934483cf21fad3f2e760f52608d19ed
TiagoCabral <tiago.gnr1985@gmail.com>
2019-04-01 18:27:07 -0300

    .

---
+++ src/main/kotlin/com/pms/placemanagementsystem/PlaceManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.pms.placemanagementsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlaceManagementSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlaceManagementSystemApplication>(*args)
+}
