70617da814fe75f958436b37c4552fbc8a5642eb
TiagoCabral <tiago.gnr1985@gmail.com>
2019-04-03 19:07:43 -0300

    teste push

--- src/main/kotlin/com/pms/placemanagementsystem/PlaceManagementSystemApplication.kt
+++ src/main/kotlin/com/pms/placemanagementsystem/PlaceManagementSystemApplication.kt
@@ -2,10 +2,12 @@ package com.pms.placemanagementsystem
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.logging.Logger
 
 @SpringBootApplication
 class PlaceManagementSystemApplication
 
 fun main(args: Array<String>) {
 	runApplication<PlaceManagementSystemApplication>(*args)
+	print("Teste")
 }
