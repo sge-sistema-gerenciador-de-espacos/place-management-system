384b1b212c94f164929dbf5a1818de57d20039db
Tiago Cabral <tiago.gnr1985@gmail.com>
2019-04-07 15:30:52 -0300

    Adds annotations (Commented) from jpa.

--- src/main/kotlin/com/pms/placemanagementsystemserverside/models/SpaceModel.kt
+++ src/main/kotlin/com/pms/placemanagementsystemserverside/models/SpaceModel.kt
@@ -2,5 +2,24 @@ package com.pms.placemanagementsystemserverside.models
 
 import com.pms.placemanagementsystemserverside.models.enums.TypeOfSpaceEnum
 
-open class SpaceModel(private val name: String, private val numberOfChairs: Int,
-                 private val hasProjector: Boolean, private val typeOfSpace: TypeOfSpaceEnum)
+//@Entity
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+open class SpaceModel(
+
+//        @Id
+//        @GeneratedValue(strategy = GenerationType.AUTO)
+//        @Column(name = "id", updatable = false, nullable = false)
+        private val id: Long,
+
+//        @Column
+        private val name: String,
+
+//        @Column
+        private val numberOfChairs: Int,
+
+//        @Column
+        private val hasProjector: Boolean,
+
+//        @Column
+        private val typeOfSpace: TypeOfSpaceEnum
+)
