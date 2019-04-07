4886b7aba3eaf630bad506af3af6dcfcc7c1c64a
Tiago Cabral <tiago.gnr1985@gmail.com>
2019-04-07 15:44:59 -0300

    Deletes ClassRoomModel

--- src/main/kotlin/com/pms/placemanagementsystemserverside/models/ClassRoomModel.kt
+++
@@ -1,8 +0,0 @@
-package com.pms.placemanagementsystemserverside.models
-
-import com.pms.placemanagementsystemserverside.models.enums.TypeOfSpaceEnum
-
-data class ClassRoomModel(private val id: Long, private val name: String, private val numberOfChairs: Int,
-                          private val hasProjector: Boolean, private val hasBoard: Boolean,
-                          private val hasSmartBoard: Boolean)
-    : SpaceModel(id, name, numberOfChairs, hasProjector, TypeOfSpaceEnum.CLASSROOM)
