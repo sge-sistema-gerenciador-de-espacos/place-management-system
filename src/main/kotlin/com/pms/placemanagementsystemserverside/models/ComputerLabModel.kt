cf68c5e08acd0dcae07f79e855d954f87f3e1b9f
TiagoCabral <tiago.gnr1985@gmail.com>
2019-04-08 12:15:28 -0300

    Init properties.

--- src/main/kotlin/com/pms/placemanagementsystemserverside/models/ComputerLabModel.kt
+++ src/main/kotlin/com/pms/placemanagementsystemserverside/models/ComputerLabModel.kt
@@ -4,7 +4,7 @@ import com.pms.placemanagementsystemserverside.models.enums.TypeOfSpaceEnum
 
 class ComputerLabModel(id: Long, name: String, numberOfChairs: Int,
                        hasProjector: Boolean, hasBoard: Boolean,
-                       hasSmartBoard: Boolean, var numberOfPcs: Int,
-                       var softwares: MutableList<SoftwareModel>)
+                       hasSmartBoard: Boolean, var numberOfPcs: Int = 0,
+                       var softwares: MutableList<SoftwareModel> = mutableListOf())
     : SpaceModel(id = id, name = name, numberOfChairs = numberOfChairs, hasProjector = hasProjector,
         hasBoard = hasBoard, hasSmartBoard = hasSmartBoard, typeOfSpace = TypeOfSpaceEnum.COMPUTER_LAB)
