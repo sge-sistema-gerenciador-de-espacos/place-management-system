ad5bfdddccb5ec51c861cec669a35e3dcf66c49f
Tiago Cabral <tiago.gnr1985@gmail.com>
2019-04-20 18:05:20 -0300

    Deletes class.

--- src/main/kotlin/com/pms/placemanagementsystemserverside/apicontroller/BaseApiController.kt
+++
@@ -1,8 +0,0 @@
-package com.pms.placemanagementsystemserverside.apicontroller
-
-import com.pms.placemanagementsystemserverside.apicontroller.contract.ApiController
-import org.springframework.web.bind.annotation.RequestMapping
-
-@RequestMapping(consumes = ["application/json"],
-        produces = ["application/json"])
-abstract class BaseApiController<T> : ApiController<T>
