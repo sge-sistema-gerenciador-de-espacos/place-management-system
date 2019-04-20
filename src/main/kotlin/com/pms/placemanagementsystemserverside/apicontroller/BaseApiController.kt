33dcef8fc91b919347ef3f5ecc8baf2e68959800
Tiago Cabral <tiago.gnr1985@gmail.com>
2019-04-20 13:48:15 -0300

    Adds consumes and produces type for @RequestMapping.

--- src/main/kotlin/com/pms/placemanagementsystemserverside/apicontroller/BaseApiController.kt
+++ src/main/kotlin/com/pms/placemanagementsystemserverside/apicontroller/BaseApiController.kt
@@ -2,8 +2,7 @@ package com.pms.placemanagementsystemserverside.apicontroller
 
 import com.pms.placemanagementsystemserverside.apicontroller.contract.ApiController
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
-@RequestMapping(value = ["/pms-api"])
+@RequestMapping(consumes = ["application/json"],
+        produces = ["application/json"])
 abstract class BaseApiController<T> : ApiController<T>
