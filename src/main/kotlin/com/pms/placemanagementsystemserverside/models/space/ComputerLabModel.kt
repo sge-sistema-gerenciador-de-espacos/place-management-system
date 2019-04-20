d5aa78f5166aecf4741930b357ba3af6fc9abed2
Tiago Cabral <tiago.gnr1985@gmail.com>
2019-04-20 14:59:30 -0300

    Changes to data class.

--- src/main/kotlin/com/pms/placemanagementsystemserverside/models/space/ComputerLabModel.kt
+++ src/main/kotlin/com/pms/placemanagementsystemserverside/models/space/ComputerLabModel.kt
@@ -2,10 +2,16 @@ package com.pms.placemanagementsystemserverside.models.space
 
 import com.pms.placemanagementsystemserverside.models.enums.TypeOfSpaceEnum
 
-class ComputerLabModel(id: Long, name: String, numberOfChairs: Int,
-                       hasProjector: Boolean, hasBoard: Boolean,
-                       hasSmartBoard: Boolean, var numberOfPcs: Int = 0,
-                       var softwares: MutableList<SoftwareModel> = mutableListOf())
+data class ComputerLabModel(
+        override var id: Long,
+        override var name: String,
+        override var numberOfChairs: Int,
+        override var hasProjector: Boolean,
+        override var hasBoard: Boolean,
+        override var hasSmartBoard: Boolean,
+        var numberOfPcs: Int = 0,
+        var softwares: MutableList<SoftwareModel> = mutableListOf()
+)
 
     : SpaceModel(id = id, name = name, numberOfChairs = numberOfChairs, hasProjector = hasProjector,
         hasBoard = hasBoard, hasSmartBoard = hasSmartBoard, typeOfSpace = TypeOfSpaceEnum.COMPUTER_LAB)
