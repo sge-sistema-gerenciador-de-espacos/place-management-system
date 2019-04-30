1a98995ed5d10d8425eb9f8d976009f5b8f15843
Tiago Cabral <tiago.gnr1985@gmail.com>
2019-04-30 01:20:19 -0300

    Deletes unnecessary interface.

--- src/main/kotlin/com/pms/placemanagementsystemserverside/repository/contract/Repository.kt
+++
@@ -1,8 +0,0 @@
-package com.pms.placemanagementsystemserverside.repository.contract
-
-interface Repository<T> {
-    fun create(item: T)
-    fun read(item: T): List<T>
-    fun update(item: T)
-    fun delete(item: T)
-}
