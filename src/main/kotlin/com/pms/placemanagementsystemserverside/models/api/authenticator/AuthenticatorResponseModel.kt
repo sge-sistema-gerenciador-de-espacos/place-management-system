6eb73da8a4de98f314dfc3eb1b5a23947065e31b
TiagoCaborsa <tiago.gnr1985@gmail.com>
2019-10-10 02:17:40 -0300

    Improves.

--- src/main/kotlin/com/pms/placemanagementsystemserverside/models/api/authenticator/AuthenticatorResponseModel.kt
+++ src/main/kotlin/com/pms/placemanagementsystemserverside/models/api/authenticator/AuthenticatorResponseModel.kt
@@ -2,6 +2,5 @@ package com.pms.placemanagementsystemserverside.models.api.authenticator
 
 //TODO adc anotacoes do jpa
 data class AuthenticatorResponseModel(
-        var user: String = "Anthony",
-        var token: String = "F724G284HF84G0I80YT0284U3"
+        var token: String = "admin-token"
 )
